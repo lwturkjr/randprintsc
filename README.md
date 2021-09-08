df29b7179c0e4fef04643521841827ae8c7346cf
Lloyd W Turk Jr <31788118+toastyxen@users.noreply.github.com>
2021-09-07 21:55:11 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# A script to open a random image on print.sc using selenium, simage sharing for screenshot software lightshot
+You need python and pip installed
+
+pip install -r requirements.txt #Install dependencies
+
+Just something fun, maybe find something interesting
+
+python main.py
---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import string
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+
+options = webdriver.ChromeOptions() 
+options.add_argument("window-size=1280,800")
+options.add_argument('--disable-blink-features=AutomationControlled')
+browser  = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+browser.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+#print(browser.execute_script("return navigator.userAgent;"))
+
+letters = string.ascii_lowercase
+numbers = string.digits
+
+lets = ''.join(random.choice(letters) for i in range(2))
+nums = ''.join(random.choice(numbers) for i in range(4))
+
+img = lets + nums
+
+# Open the Website
+browser.get("https://prnt.sc/"+img)
+
